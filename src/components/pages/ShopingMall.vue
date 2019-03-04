be4bf14efcb4e2ac301731fcb827579473e26209
qinqiang <qinqiang@chuchujie.com>
2019-03-04 23:19:19 +0800

    koa demo

---
+++ src/components/pages/ShopingMall.vue
@@ -0,0 +1,70 @@
+<template>
+<div>
+    <div class="search-bar">
+        <van-row gutter="5">
+            <van-col span="3">
+                <img :src="locationIcon" width="80%" class="location-icon">  
+
+            </van-col>
+            <van-col span="16">
+                <input type="text" class="search-input"/>
+            </van-col>
+            <van-col span="5">
+                <van-button type="default" width="100%">搜索</van-button>
+            </van-col>
+        </van-row>
+     </div>
+    <!-- swiper -->
+    <div class="swiper-area">
+    <van-swipe :autoplay="1000">
+        <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
+            <img :src="banner.imageUrl" width="100%"/>
+        </van-swipe-item>
+    </van-swipe>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'shopApp',
+                locationIcon:require('../../assets/images/location.png'),
+                 bannerPicArray:[
+                    {imageUrl:'http://h.hiphotos.baidu.com/image/pic/item/cefc1e178a82b901fd40c8077d8da9773912ef11.jpg'},
+                    {imageUrl:'http://b.hiphotos.baidu.com/image/pic/item/78310a55b319ebc4cee036bd8c26cffc1e17167d.jpg'},
+                    {imageUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552315885&di=a985080247548e256fc87a6212ebef0c&imgtype=jpg&er=1&src=http%3A%2F%2Fimage.17173.com%2Fbbs%2Fv1%2F2010%2F09%2F08%2F1283947526539.jpg'},
+                 ]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+ .search-bar{
+      height: 2.2rem;
+      background-color: #e5017d;
+      line-height:2.2rem;
+      overflow: hidden;
+  }
+.search-input{
+      width:100%;
+      height: 1.3rem;
+      border-top:0px;
+      border-left:0px;
+      border-right:0px;
+      border-bottom: 1px solid 1px !important ;
+      background-color: #e5017d;
+      color:#fff;
+  }
+  .location-icon{
+      padding-top: .2rem;
+      padding-left: .3rem;
+  }
+  .swiper-area{
+      clear: both;
+      max-height: 10rem;
+      overflow: hidden;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ShopingMall from '@/components/pages/ShopingMall'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/ShopingMall',
+      name: 'ShopingMall',
+      component: ShopingMall
     }
   ]
 })

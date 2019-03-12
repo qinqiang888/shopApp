365c9cd2a6907763a3532779bd91d58c6bd6688d
秦强 <qinqiang@chuchujie.com>
2019-03-12 09:53:28 +0800

    接口路由

--- service/index.js
+++ service/index.js
@@ -13,19 +13,19 @@ App.use(async(res)=>{
     res.body='<h1>my first Koa2 api </h1>'
 
 })
-;(async()=>{
-    await connect()
-    initSchemas();
-    const userInfo = mongoose.model('userInfo')
-    let oneUser =new userInfo({username:'admin',password:'123456',userage:27})
-    oneUser.save().then(()=>{
-        console.log('插入成功')
-    })
-    let user=await userInfo.findOne({username:'mary'}).exec()
-    console.log('------------------------------')
-    console.log(user)
+// ;(async()=>{
+//     await connect()
+//     initSchemas();
+//     const userInfo = mongoose.model('userInfo')
+//     let oneUser =new userInfo({username:'admin',password:'123456',userage:27})
+//     oneUser.save().then(()=>{
+//         console.log('插入成功')
+//     })
+//     let user=await userInfo.findOne({username:'mary'}).exec()
+//     console.log('------------------------------')
+//     console.log(user)
 
-})()
+// })()
 App.listen(3000,()=>{
     console.log('--------服务启动成功---------')
 })

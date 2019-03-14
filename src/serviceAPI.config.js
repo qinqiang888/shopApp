f22cce73e3e2ed406af163d77092bf273be4f0b2
秦强 <qinqiang@chuchujie.com>
2019-03-14 10:21:42 +0800

    完善

--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -5,10 +5,6 @@ const URL ={
     getGoodsInfo : BASEURL+'getGoodsInfo', 
     registerUser : LOCALURL+'user/register',  //用户注册接口
     login : LOCALURL+'user/login',  //用户登录接口
-    getDetailGoodsInfo : LOCALURL+'goods/getDetailGoodsInfo',  //获取商品详情
-    getCateGoryList : LOCALURL+'goods/getCateGoryList',  //得到大类信息
-    getCateGorySubList : LOCALURL+'goods/getCategorySubList',  //得到小类信息
-    getGoodsListByCategorySubID : LOCALURL+'goods/getGoodsListByCategorySubID',  //得到小类商品信息
 }
 
 export default URL;
